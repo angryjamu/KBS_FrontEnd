4aa240166a965a556e0c8b877c67dc7915af27eb
kangboseong <vlwktksek1@naver.com>
2021-12-22 10:52:04 +0900

    l

---
+++ 001.HTML/008.VOGUE_PJ/002.site/js/gallery.js
@@ -0,0 +1,18 @@
+// 보그 PJ 갤러리 JS - gallery.js
+
+///// 로딩구역 /////////////
+$(()=>{ // jQB ////////////////////////////////
+
+    console.log("갤러리 로딩완료!")
+
+    let swiper = new Swiper('.swiper-container', {
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        autoplay: {
+            delay: 2000,
+          },
+      });
+
+}); ///////////////// jQB /////////////////////
+///////////////////////////////////////////////
