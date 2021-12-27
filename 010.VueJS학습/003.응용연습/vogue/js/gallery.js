d7d65030861323a25644fe15986c2d8c49a3b4e5
kangboseong <vlwktksek1@naver.com>
2021-12-27 15:32:41 +0900

    a

---
+++ 010.VueJS학습/003.응용연습/vogue/js/gallery.js
@@ -0,0 +1,29 @@
+// 보그 PJ 갤러리 JS - gallery.js
+
+///// 로딩구역 /////////////
+$(() => { // jQB ////////////////////////////////
+
+    console.log("갤러리 로딩완료!")
+
+    // 스와이퍼 플러그인 적용하기 //
+    let swiper = new Swiper(".mySwiper", {
+
+
+        
+        loop: true, // 무한루프
+        pagination: { // 하단블릿
+            el: ".swiper-pagination", // 블릿대상박스
+            clickable: true, // 블릿 클릭 가능여부
+        },
+        navigation: { // 양쪽이동버튼
+            nextEl: ".swiper-button-next", // 다음버튼대상
+            prevEl: ".swiper-button-prev", // 이전버튼대상
+        },
+        autoplay: { // 자동넘김
+            delay: 2000, // 사이시간
+            disableOnInteraction: false, // 상호작용이 없을때 재가동
+        },
+    }); ///////// swiper ///////////////
+
+}); ///////////////// jQB /////////////////////
+///////////////////////////////////////////////
