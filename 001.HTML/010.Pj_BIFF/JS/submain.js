5fc739bd3946eaa4c757eb1a6d8ab7ee9e7225ac
kangboseong <vlwktksek1@naver.com>
2024-01-25 18:16:26 +0900

    s

--- 001.HTML/010.Pj_BIFF/JS/submain.js
+++ 001.HTML/010.Pj_BIFF/JS/submain.js
@@ -1,59 +1,46 @@
-/// 햄버거메뉴 JS ////////////////////////////
+// 햄버거메뉴
 function sMenu() {
 
   let gnb1 =
     document.getElementsByClassName("gnb1").item(0);
   gnb1.classList.toggle("on");
 
+  document.querySelectorAll('.gnb1 a').forEach(function (link) {
+    link.addEventListener('click', function () {
+        document.querySelector('.gnb1').classList.remove('on');
+    });
+  });
 };
 
-document.querySelectorAll('.gnb1 a').forEach(function (link) {
-  link.addEventListener('click', function () {
-      // .gnb1에게 "on" 클래스 제거
-      document.querySelector('.gnb1').classList.remove('on');
-  });
+// 오디오
+let audio = document.getElementById('bgm');
+let waveAnimation = document.getElementById('wave-animation');
+let audiotxt = document.getElementsByClassName('audiotxt');
+let isPlaying = false;
+
+function toggleAudio() {
+    if (isPlaying) {
+        audio.pause();
+        waveAnimation.style.animationPlayState = 'paused';
+        waveAnimation.style.transform = 'translate(-50%, -50%) scale(1)';
+        waveAnimation.style.opacity = 0.8;
+        audiotxt[0].innerText = 'BGM Play!';
+    } else {
+        audio.play();
+        waveAnimation.style.animationPlayState = 'running';
+        waveAnimation.style.opacity = 0.8;
+        audiotxt[0].innerText = 'BGM Stop!';
+    }
+    isPlaying = !isPlaying;
+}
+
+audio.addEventListener('ended', function() {
+    // Reset animation when audio playback ends
+    waveAnimation.style.animationPlayState = 'paused';
+    waveAnimation.style.transform = 'translate(-50%, -50%) scale(1)';
+    waveAnimation.style.opacity = 0.8;
+    isPlaying = false;
 });
-/// 햄버거메뉴 JS //////////////////////////
-
-// 로딩
-document.addEventListener('DOMContentLoaded', function () {
-  var video = document.getElementsByClassName('Mvideo');
-
-  video.addEventListener('loadeddata', function () {
-      // 비디오가 로드된 후 실행되는 코드
-      console.log('비디오 로딩 완료');
-  });
-});
-
-            //audio script
-            let audio = document.getElementById('bgm');
-            let waveAnimation = document.getElementById('wave-animation');
-            let audiotxt = document.getElementsByClassName('audiotxt');
-            let isPlaying = false;
-    
-            function toggleAudio() {
-                if (isPlaying) {
-                    audio.pause();
-                    waveAnimation.style.animationPlayState = 'paused';
-                    waveAnimation.style.transform = 'translate(-50%, -50%) scale(1)';
-                    waveAnimation.style.opacity = 0.8;
-                    audiotxt[0].innerText = 'BGM Play!';
-                } else {
-                    audio.play();
-                    waveAnimation.style.animationPlayState = 'running';
-                    waveAnimation.style.opacity = 0.8;
-                    audiotxt[0].innerText = 'BGM Stop!';
-                }
-                isPlaying = !isPlaying;
-            }
-    
-            audio.addEventListener('ended', function() {
-                // Reset animation when audio playback ends
-                waveAnimation.style.animationPlayState = 'paused';
-                waveAnimation.style.transform = 'translate(-50%, -50%) scale(1)';
-                waveAnimation.style.opacity = 0.8;
-                isPlaying = false;
-            });
 
 
 // swiper
