5b9980e97a598e46589623469eeacbed02459c84
kangboseong <vlwktksek1@naver.com>
2024-01-28 14:44:27 +0900

    j

--- 001.HTML/012.Blue_PJ/JS/mian.js
+++ 001.HTML/012.Blue_PJ/JS/mian.js
@@ -1,6 +1,21 @@
+// 메뉴버튼
 $(function(){
     $('.menuBtn').click(function(){
         $('.wrapBox.on').toggleClass('on')
         console.log(this)
     })
 })
+
+// 스와이퍼
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
