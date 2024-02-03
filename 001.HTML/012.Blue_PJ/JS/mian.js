6b356df37e8e812ade69738bd1eb36bbf3ece4ed
kangboseong <vlwktksek1@naver.com>
2024-02-03 19:53:53 +0900

    k

--- 001.HTML/012.Blue_PJ/JS/mian.js
+++ 001.HTML/012.Blue_PJ/JS/mian.js
@@ -1,9 +1,30 @@
 // 메뉴버튼
-$(function(){
+$(function() {
+    // 기존 이미지 저장
+    var originalImgSrc = $('.ham .imgCont img').attr('src');
+
+    $('.ham ul li').hover(
+        // 마우스를 올렸을 때
+        function() {
+            var imagePath = $(this).data('image');
+
+            // 이미지 변경
+            $('.ham .imgCont img').fadeOut(400, function() {
+                $(this).attr('src', imagePath).fadeIn(400);
+            });
+        },
+        // 마우스를 뗄 때
+        function() {
+            // 기존 이미지로 되돌리기
+            $('.ham .imgCont img').fadeOut(400, function() {
+                $(this).attr('src', originalImgSrc).fadeIn(400);
+            });
+        }
+    );
+
     $('.menuBtn').click(function(){
         $('.ham').addClass('on')
     });
-    
     
     $('#exit').click(function() {
         $('.ham').removeClass('on');
